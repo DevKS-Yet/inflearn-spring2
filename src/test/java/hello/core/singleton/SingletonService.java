36728e2192a12b8daccb0b70f8083787cee17716
DevKS-Yet <ksang0324@gmail.com>
2021-12-19 16:41:58 +0900

    20211219-싱글톤패턴

---
+++ src/test/java/hello/core/singleton/SingletonService.java
@@ -0,0 +1,18 @@
+package hello.core.singleton;
+
+public class SingletonService {
+
+    // static - 자바 기본이니 따로 공부
+    private static final SingletonService instance = new SingletonService();
+
+    public static SingletonService getInstance() {
+        return instance;
+    }
+
+    private SingletonService() {}
+
+    public void logic() {
+        System.out.println("싱글톤 객체 로직 호출출");
+   }
+
+}
